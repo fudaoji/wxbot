e4f6848c01a754ed7b5fa1419b3d5283563bb7b8
fdj <fdj@kuryun.cn>
2023-05-24 15:20:00 +0800

    update addons

--- app/addon/demo/route/route.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-use think\facade\Route;
-
-Route::rule('demo/admin/index/index',"\app\addons\demo\admin\controller\Index::index");
-Route::rule('demo/admin/index/detail',"\app\addons\demo\admin\controller\Index::detail");

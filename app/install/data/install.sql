6a11b2c896f1ff29e848f20c8d0f044cd7987c7a
fdj <fdj@kuryun.cn>
2023-06-21 15:12:30 +0800

    update

--- app/install/data/install.sql
+++ app/install/data/install.sql
@@ -1189,3 +1189,7 @@ CREATE TABLE `__PREFIX__addon` (
 -- 2.2.3 手动更新请务必手动替换__PREFIX__为你的真实表前缀
 -- ----------------------------
 ALTER TABLE `__PREFIX__admin_group` ADD COLUMN `addons` text NULL COMMENT '角色应用' AFTER `tenant_group`;
+-- ----------------------------
+-- 2.2.4 手动更新请务必手动替换__PREFIX__为你的真实表前缀
+-- ----------------------------
+ALTER TABLE `__PREFIX__addon` DROP COLUMN `groups`, ADD COLUMN `sort_reply` int(10) UNSIGNED NOT NULL DEFAULT 1 AFTER `cates`;
--- upgrade.sql
+++ upgrade.sql
@@ -1,5 +1,4 @@
 -- ----------------------------
 -- 2.2.4 手动更新请务必手动替换__PREFIX__为你的真实表前缀
 -- ----------------------------
-
 ALTER TABLE `__PREFIX__addon` DROP COLUMN `groups`, ADD COLUMN `sort_reply` int(10) UNSIGNED NOT NULL DEFAULT 1 AFTER `cates`;

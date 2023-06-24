db94e289824a5ed88f3984f3a69ef9ad1f463578
fdj <fdj@kuryun.cn>
2023-06-24 20:21:12 +0800

    fix install bugs

--- app/install/data/install.sql
+++ app/install/data/install.sql
@@ -1192,4 +1192,4 @@ ALTER TABLE `__PREFIX__admin_group` ADD COLUMN `addons` text NULL COMMENT '角�
 -- ----------------------------
 -- 2.2.4 手动更新请务必手动替换__PREFIX__为你的真实表前缀
 -- ----------------------------
-ALTER TABLE `__PREFIX__addon` DROP COLUMN `groups`, ADD COLUMN `sort_reply` int(10) UNSIGNED NOT NULL DEFAULT 1 AFTER `cates`;
+ALTER TABLE `__PREFIX__addon` ADD COLUMN `sort_reply` int(10) UNSIGNED NOT NULL DEFAULT 1 AFTER `cates`;
--- upgrade.sql
+++ upgrade.sql
@@ -1,4 +1,5 @@
 -- ----------------------------
 -- 2.2.4 手动更新请务必手动替换__PREFIX__为你的真实表前缀
 -- ----------------------------
-ALTER TABLE `__PREFIX__addon` DROP COLUMN `groups`, ADD COLUMN `sort_reply` int(10) UNSIGNED NOT NULL DEFAULT 1 AFTER `cates`;
+
+ALTER TABLE `__PREFIX__addon` ADD COLUMN `sort_reply` int(10) UNSIGNED NOT NULL DEFAULT 1 AFTER `cates`;

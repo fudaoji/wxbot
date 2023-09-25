ef222f1189443ca809e812272f88b9349e71541f
fdj <fdj@kuryun.cn>
2023-09-25 08:49:50 +0800

    fix bugs

--- config/app.php
+++ config/app.php
@@ -15,7 +15,7 @@
 
 return [
     // 应用地址
-    'version' => '2.2.8',
+    'version' => '2.2.9',
     'app_host' => env('app.host', ''),
     'app_key' => env('app_key', '123456'),
     'app_name'               => 'WxBot',

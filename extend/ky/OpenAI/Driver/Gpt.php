8dde92ed88723ccbd788cd84c0925edead54c363
daniel肥杰 <87821@qq.com>
2023-11-07 06:23:27 +0000

    update extend/ky/OpenAI/Driver/Gpt.php. gpt-4-1106-preview 最新gpt-4模型
    Signed-off-by: daniel肥杰 <87821@qq.com>

--- extend/ky/OpenAI/Driver/Gpt.php
+++ extend/ky/OpenAI/Driver/Gpt.php
@@ -28,6 +28,7 @@ class Gpt extends Base
         "gpt-3.5-turbo-0613" => "gpt-3.5-turbo-0613",
         "gpt-3.5-turbo-16k" => "gpt-3.5-turbo-16k",
         "gpt-3.5-turbo-16k-0613" => "gpt-3.5-turbo-16k-0613",
+        "gpt-4-1106-preview" => "gpt-4-1106-preview",
         "gpt-4" => "gpt-4",
         "gpt-4-0613" => "gpt-4-0613",
         "gpt-4-32k" => "gpt-4-32k",
